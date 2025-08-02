8b8b24324c1ade5a886313c777243b6932db6ab0
carrada <carrada@ciencias.unam.mx>
2025-08-01 23:47:36 -0600

    Primer commit landing page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="veraz-logo-container">
+      <img src="/veraznegrologo.png" alt="Logo Veraz" className="veraz-logo" />
+    </div>
+  )
+}
+
+export default App
