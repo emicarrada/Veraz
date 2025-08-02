b930e98681be07700cf83e8f7755c2f0425fd1d5
carrada <carrada@ciencias.unam.mx>
2025-08-02 02:25:26 -0600

    background inicial

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import './App.css'
 
 
 function App() {
-  return (
-    <div className="veraz-logo-container">
-      <img src="/veraznegrologo.png" alt="Logo Veraz" className="veraz-logo" />
-    </div>
-  )
+  return (<></>);
 }
+
 
 export default App
